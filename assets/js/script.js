61cfb835f854106cbd513eed0f44ffbc08c8af83
reggietenk <reggietenkorang@gmail.com>
2022-01-09 16:17:29 -0500

    made changes to the forecast container

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,17 @@ let cuurentTempEl = document.querySelector(".temp")
 let currentWindEl = document.querySelector(".wind")
 let currentHumidity = document.querySelector(".humidity")
 let currentIndex = document.querySelector(".uv-index")
-let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=9ed2cd13892f5f7331d217b18804ba3f"
 
 
+button.addEventListener("click", function() {
+let apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"  
 fetch(apiUrl)
 .then(response => response.json())
 .then(data => console.log(data))
 
+let cityName = data['name']
+let temp = data['main']['temp']
+cityName.innerHTML=cityName
 
-
+.catch(error => alert("Invalid city name!"))
+})
