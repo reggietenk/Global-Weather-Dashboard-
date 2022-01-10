4a98f4e7e843c51c714d87ad057109d6edfb6abe
reggietenk <reggietenkorang@gmail.com>
2022-01-09 23:58:10 -0500

    style changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Global-Weather-Dashboard-
+
+## Project Goal 
+
+### Create weather dashboard to access current weather based on location
+
+## This application was created with Javascrpit, HTML, CSS, & Bootstrap  
+
+## Created By Reggie Tenkorang
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,52 @@
 let button = document.querySelector(".button")
-let cityChoiceEl = document.querySelector (".city-input")
+let cityInput = document.querySelector (".city-input")
 let currentDate =document.querySelector (".date")
 let cityEl = document.querySelector(".city-name")
-let cuurentTempEl = document.querySelector(".temp")
+let currentTempEl = document.querySelector(".temp")
 let currentWindEl = document.querySelector(".wind")
 let currentHumidity = document.querySelector(".humidity")
 let currentIndex = document.querySelector(".uv-index")
+let searchHistoryEl = document.querySelector(".search-history")
+let apiKey = "9ed2cd13892f5f7331d217b18804ba3f" 
+let apiUrl ="https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid={apiKey}"  
 
 
-button.addEventListener("click", function() {
-let apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"  
+
+button.addEventListener("click", function(event){
+    event.preventDefault
 fetch(apiUrl)
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    let citychoice = data['name'];
+    let temp = data['main']['temp'];
+    let current = data['weather'][0]['description'];
 
-let cityName = data['name']
-let temp = data['main']['temp']
-cityName.innerHTML=cityName
-
-.catch(error => alert("Invalid city name!"))
+    cityEl.innerHTML = citychoice
+    currentTempEl.innerHTML = temp
+    currentWindEl.innerHTML = current
+});
 })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+// let searchHistory = document.createElement("li")
+// searchHistoryEl.appendChild(searchHistory)
+
+
+// let apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon +"&units=imperial&appid=9ed2cd13892f5f7331d217b18804ba3f"  
+// fetch(apiUrl).then(function(response){
+//     (response).json().then(function(data){
+//      console.log(data);
+//     });
+// });
+
